a8b926aa0bac189efe27001c2cf731b9741a9e5a
Jorge-Sanchez-dev <jorgesl099@gmail.com>
2025-11-24 19:27:07 +0100

    Algunos cambios

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-// index.ts
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
@@ -8,32 +7,24 @@ import authRoutes from "./routes/auth";
 
 dotenv.config();
 
-console.log(">>> Entrando en index.ts");
-
 const app = express();
 app.use(express.json());
 
-// servir frontend
+
 app.use(express.static(path.join(process.cwd(), "public")));
 
-// rutas API
+// Rutas API
 app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 (async () => {
   try {
-    console.log(">>> Llamando a connectMongoDB...");
     await connectMongoDB();
-    console.log(">>> Mongo OK, levantando servidor...");
 
     app.listen(PORT, () => {
-      console.log(`Servidor en http://localhost:${PORT}`);
+      console.log(`Servidor listo en http://localhost:${PORT}`);
     });
-
-    app.get('/', (req, res) => {
-  res.send('Servidor funcionando ✅');
-});
 
   } catch (err) {
     console.error("Error al iniciar la app:", err);
